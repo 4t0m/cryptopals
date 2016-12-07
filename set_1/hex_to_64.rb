f1a320adf0ec5993a3ec05ebf58910ac8e53cbd6
Adom Hartell <adomhartell@gmail.com>
2016-12-06 20:37:15 -0800

    stop using built-in hex-to-int conversion

--- set_1/hex_to_64.rb
+++ set_1/hex_to_64.rb
@@ -1,27 +1,15 @@
 require 'base64'
 
-b64_values = (0..63).to_a
-b64_strings = ('A'..'Z').to_a +
-              ('a'..'z').to_a +
-              (0..9).to_a.map(&:to_s) +
-              ['+', '/']
 
-BASE64_ENCODE_HASH = {}
-
-(0...64).each do |index|
-  BASE64_ENCODE_HASH[b64_values[index]] = b64_strings[index]
-end
-
-BASE64_ENCODE_HASH.freeze
 
 def hex_to_64(hex_string)
   bin = hex_to_bin(hex_string)
-  p bin_to_64(bin)
+  bin_to_64(bin)
 end
 
 def hex_to_bin(hex_string)
-  hex_string.chars.map do |x|
-    bin_str = x.hex.to_s(2)
+  hex_string.chars.map do |hex|
+    bin_str = HEX_DECODE_HASH[hex].to_s(2)
     bin_str = "0#{bin_str}" until bin_str.length == 4
     bin_str
   end.join
@@ -36,6 +24,40 @@ def bin_to_64(bin_string)
     BASE64_ENCODE_HASH[int_val]
   end.join
 end
+
+private
+
+HEX_DECODE_HASH = { '0' => 0,
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                    '4' => 4,
+                    '5' => 5,
+                    '6' => 6,
+                    '7' => 7,
+                    '8' => 8,
+                    '9' => 9,
+                    'a' => 10,
+                    'b' => 11,
+                    'c' => 12,
+                    'd' => 13,
+                    'e' => 14,
+                    'f' => 15 }.freeze
+
+b64_values = (0..63).to_a
+b64_strings = ('A'..'Z').to_a +
+              ('a'..'z').to_a +
+              (0..9).to_a.map(&:to_s) +
+              ['+', '/']
+
+BASE64_ENCODE_HASH = {}
+
+(0...64).each do |index|
+  BASE64_ENCODE_HASH[b64_values[index]] = b64_strings[index]
+end
+
+BASE64_ENCODE_HASH.freeze
+
 
 # TEST CODE
 test_hex = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
