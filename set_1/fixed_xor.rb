ce860fe801e7833f9ca1dec046b7a4110267e365
Adom Hartell <adomhartell@gmail.com>
2016-12-07 13:15:00 -0800

    first draft of fixed_xor.rb

--- set_1/fixed_xor.rb
+++ set_1/fixed_xor.rb
@@ -2,15 +2,17 @@ require_relative 'hex_to_64'
 
 def fixed_xor(string_1, string_2)
   bin_1, bin_2 = hex_to_bin(string_1), hex_to_bin(string_2)
-  p bin_1
-  p bin_2
-  bin_xor =  (0...bin_1.length).inject("") do |result, index|
-    result + (bin_1[index] == bin_2[index] ? '0' : '1')
-  end
-  p bin_xor 
-  bin_xor.to_i(16)
+  xor(bin_1, bin_2)
 end
 
+def xor(bin_1, bin_2)
+  result = ""
+  (0...bin_1.length).each do |index|
+    result += (bin_1[index] == bin_2[index] ? "0" : "1")
+  end
+  
+  result 
+end
 # test code
 string_1 = '1c0111001f010100061a024b53535009181c'
 string_2 = '686974207468652062756c6c277320657965'
