78e154f460e0babfeeff4e3eee55ef45127b2eb3
Adom Hartell <adomhartell@gmail.com>
2016-12-07 13:26:13 -0800

    complete fixed_xor using to_i and to_s methods

--- set_1/fixed_xor.rb
+++ set_1/fixed_xor.rb
@@ -2,10 +2,11 @@ require_relative 'hex_to_64'
 
 def fixed_xor(string_1, string_2)
   bin_1, bin_2 = hex_to_bin(string_1), hex_to_bin(string_2)
-  xor(bin_1, bin_2)
+  bin_str = bin_xor(bin_1, bin_2)
+  bin_to_hex(bin_str)
 end
 
-def xor(bin_1, bin_2)
+def bin_xor(bin_1, bin_2)
   result = ""
   (0...bin_1.length).each do |index|
     result += (bin_1[index] == bin_2[index] ? "0" : "1")
@@ -13,6 +14,17 @@ def xor(bin_1, bin_2)
   
   result 
 end
+
+def bin_to_hex(bin_str)
+  hex_str = ""
+  bin_str.chars.each_slice(4) do |nibble|
+    hex_str << nibble.join.to_i(2).to_s(16)
+  end
+
+  hex_str
+end 
+    
+
 # test code
 string_1 = '1c0111001f010100061a024b53535009181c'
 string_2 = '686974207468652062756c6c277320657965'
